26d9dfda3f5267223683e7399626a5907b6503f4
Nik McFly <70242477+nikmcfly@users.noreply.github.com>
2021-05-04 03:24:44 +0200

    Add files via upload

--- background.js
+++ background.js
@@ -1,25 +1,36 @@
+const API_Table_URL = 'https://api.airtable.com/v0/appz1m76C8vzI7806/Everything';
+const API_Key = 'keydLH6vSFDd0LUhg';
+
 chrome.runtime.onMessage.addListener(function(request,sender,callback) {
 
 	if (request.msg == "CLOUTREVIEW_SAVE_REVIEW"){
 		SaveReview(request.saveObj);
 	}
+	
+	if (request.msg == "CLOUTREVIEW_SAVE_PRIVATEREVIEW"){
+		SavePrivateReview(request.saveObj);
+	}	
+	
+	if (request.msg == "CLOUTREVIEW_UPDATE_PRIVATEREVIEW"){
+		UpdatePrivateReview(request.saveObj);
+	}		
 
 	if (request.msg == "CLOUTREVIEW_GET_REVIEWS"){
 		
-		GetAllReviews(request.userNik, callback);
+		GetAllReviews(request.userNik, request.userPublicKey, callback);
 
         return true;
 	}	
 });	
 
 
-async function GetAllReviews(sNik, callback) {
+async function GetAllReviews(sNik, sPublicKey, callback) {
 	var totalReviews = {};
 		totalReviews.records = [];
 	var sOffset = "";
 		
 	for (var i = 0; i < 100; i++){
-		var currData = await GetReviews(sNik, sOffset);	
+		var currData = await GetReviews(sNik, sPublicKey, sOffset);	
 			
 			for (var j = 0; j < currData.records.length; j++){
 				totalReviews.records.push(currData.records[j]);
@@ -37,24 +48,28 @@ async function GetAllReviews(sNik, callback) {
 }
 
 
-function GetReviews(sNik, sOffset) {
+function GetReviews(sNik, sPublicKey, sOffset) {
     return new Promise(function (resolve, reject) {
-		var sFormula = '{ReviewedUsername}="' + sNik + '"';
-        var requestUrl = "https://api.airtable.com/v0/appz1m76C8vzI7806/tblGb8lG6OxM02Hhr";
+		//var sFormula = '{ReviewedUsername}="' + sNik + '"';
+		var sFormula = '{ReviewedKey}="' + sPublicKey + '"';		
+        //var requestUrl = "https://api.airtable.com/v0/appz1m76C8vzI7806/tblGb8lG6OxM02Hhr";
+		var requestUrl = API_Table_URL;
 		requestUrl = requestUrl + "?" + "filterByFormula=" + encodeURIComponent(sFormula);
 			if (sOffset != ""){
 				requestUrl = requestUrl + "&offset=" + sOffset;
 			}
 			
 		var request = new XMLHttpRequest();		
-		request.open("GET", requestUrl, true);	
-		request.setRequestHeader("Authorization", "Bearer keykPaMI0f35E6hcW");
+		request.open("GET", requestUrl, true);
+		//request.setRequestHeader("Authorization", "Bearer keykPaMI0f35E6hcW");		
+		request.setRequestHeader("Authorization", "Bearer " + API_Key);
+		
 		request.setRequestHeader("Content-type", "application/json");
 		
 		request.onload = function () {
 			if (request.status = 200) {
 				var tobj = JSON.parse(request.responseText);
-				//console.log(tobj);
+				console.log(tobj);
 				resolve(tobj);				
 			} else {
 				console.log("Error SaveReview" + request.status + " " + request.statusText);
@@ -72,12 +87,78 @@ function GetReviews(sNik, sOffset) {
 }
 
 
-function SaveReview(saveObj){
-	var requestUrl = "https://api.airtable.com/v0/appz1m76C8vzI7806/tblGb8lG6OxM02Hhr";
+function SavePrivateReview(saveObj){
+	//var requestUrl = "https://api.airtable.com/v0/appz1m76C8vzI7806/tblGb8lG6OxM02Hhr";
+	var requestUrl = API_Table_URL;
  
 	var request = new XMLHttpRequest();
 	request.open("POST", requestUrl, true);	
-	request.setRequestHeader("Authorization", "Bearer keykPaMI0f35E6hcW");
+	//request.setRequestHeader("Authorization", "Bearer keykPaMI0f35E6hcW");
+	request.setRequestHeader("Authorization", "Bearer " + API_Key);
+	request.setRequestHeader("Content-type", "application/json");
+	
+    request.onload = function () {
+        if (request.status = 200) {
+				//var tobj = JSON.parse(request.responseText);
+        } else {
+            console.log("Error SavePrivateReview" + request.status + " " + request.statusText);
+        }		
+    }
+
+    request.onerror = function () {
+        console.log("airtable. Error while SavePrivateReview. Request failed");
+    };	
+
+	var fields_ = {};
+	fields_["Author"] = saveObj.author;
+	fields_["AuthorKey"] = saveObj.authorPublicKey;
+	fields_["ReviewedUsername"] = saveObj.reviewedusername;
+	fields_["ReviewedKey"] = saveObj.revieweduserPublicKey;
+	fields_["PrivateReview"] = saveObj.privateReviewText;
+	
+	var postbody = JSON.stringify({fields : fields_});
+  	
+	request.send(postbody);		
+}
+
+
+function UpdatePrivateReview(saveObj){
+	var requestUrl = API_Table_URL + "/" + saveObj.recordID;
+ 
+	var request = new XMLHttpRequest();
+	request.open("PATCH", requestUrl, true);	
+	request.setRequestHeader("Authorization", "Bearer " + API_Key);
+	request.setRequestHeader("Content-type", "application/json");
+	
+    request.onload = function () {
+        if (request.status = 200) {
+				//var tobj = JSON.parse(request.responseText);
+        } else {
+            console.log("Error UpdatePrivateReview" + request.status + " " + request.statusText);
+        }		
+    }
+
+    request.onerror = function () {
+        console.log("airtable. Error while UpdatePrivateReview. Request failed");
+    };	
+
+	var fields_ = {};
+	fields_["PrivateReview"] = saveObj.privateReviewText;
+	
+	var postbody = JSON.stringify({fields : fields_});
+  	
+	request.send(postbody);		
+}
+
+
+function SaveReview(saveObj){
+	//var requestUrl = "https://api.airtable.com/v0/appz1m76C8vzI7806/tblGb8lG6OxM02Hhr";
+	var requestUrl = API_Table_URL;
+ 
+	var request = new XMLHttpRequest();
+	request.open("POST", requestUrl, true);	
+	//request.setRequestHeader("Authorization", "Bearer keykPaMI0f35E6hcW");
+	request.setRequestHeader("Authorization", "Bearer " + API_Key);
 	request.setRequestHeader("Content-type", "application/json");
 	
     request.onload = function () {
@@ -94,7 +175,9 @@ function SaveReview(saveObj){
 
 	var fields_ = {};
 	fields_["Author"] = saveObj.author;
+	fields_["AuthorKey"] = saveObj.authorPublicKey;
 	fields_["ReviewedUsername"] = saveObj.reviewedusername;
+	fields_["ReviewedKey"] = saveObj.revieweduserPublicKey;
 	fields_["Rating"] = saveObj.iRating;
 	fields_["Title"] = saveObj.title;
 	fields_["PublicReview"] = saveObj.reviewtext;
--- content.js
+++ content.js
@@ -4,6 +4,11 @@ var iRating = 0;
 
 var iPreviousTab = 0;
 
+var sAuthorPublicKey = "";
+var sPrivateReviewText = "";
+var sPrivateReviewRecordID = "";
+
+
 $id = function(id){return document.getElementById(id)};
 
 function observePage(mutationRecords){
@@ -24,25 +29,58 @@ function observePage(mutationRecords){
 function ReceiveCallback(obj){
 	console.log(obj);
 	var sReviewHTML = "";
+	var sPrivateReviewHTML = ""; 
 	var raiting = 0.0;
 	var starsCount = 0;
+	var iPublicReviewCount = 0;
 	
-		var elementCurrUser = document.querySelector('#changeAccountSelectorRoot');
+		/*var elementCurrUser = document.querySelector('#changeAccountSelectorRoot');
 		var sCurrUser = "";
 		if (elementCurrUser){
 			sCurrUser = elementCurrUser.textContent.trim();
-		}	
+		}	*/
+		
+	var sCurrUser = GetLoggedUserNik();	
+		
+	var bAllowReview = ((sCurrUser != "") && (sCurrUser != GetProfileNik()));
 	
-	if (obj.records.length == 0){
-		sReviewHTML = '<div id="idReview1" style="width:100%; height:270px; text-align: center; padding-top: 50px; background: url('+ chrome.extension.getURL('images/no_reviews_yet.png') +') no-repeat center;">' +
-		'<div style="font-size: x-large; font-weight: bold; line-height: normal; margin-bottom: 20px;">No reviews yet</div>' + 
-		'<button id="idWriteFeedback" class="btn-primary" style="width:40%; height: 50px; vertical-align: middle; border-radius: 0.5em; margin-top: 15px;">Write a feedback</button>' + 
-		'</div>';		
+	if (bAllowReview){	
+		sPrivateReviewHTML = '<div id="idPrivateReview" style="width:100%; text-align: center; padding-top: 5px;">' +
+			'<textarea id="idPrivateReviewText" rows="3" placeholder="A note about this user will be visible only for you. &#13;&#10;Click to enter text" style="width:98%; overflow-y: scroll; resize: none;"></textarea>' + 
+		'</div>';	
+	}	
+	
+	for (var i = 0; i < obj.records.length; i++){
+		if (obj.records[i].fields.Rating){
+			iPublicReviewCount++;
+		} else {
+			if (obj.records[i].fields.PrivateReview){
+				sPrivateReviewText = obj.records[i].fields.PrivateReview;
+			} else {
+				sPrivateReviewText = "";
+			}			
+			sPrivateReviewRecordID = obj.records[i].id;
+		}
+	}
+	
+	
+	if (iPublicReviewCount == 0){
+		if (bAllowReview){	
+			sReviewHTML = '<div id="idReview1" style="width:100%; height:270px; text-align: center; padding-top: 50px; background: url('+ chrome.extension.getURL('images/no_reviews_yet.png') +') no-repeat center;">' +
+			'<div style="font-size: x-large; font-weight: bold; line-height: normal; margin-bottom: 20px;">No reviews yet</div>' + 
+			'<button id="idWriteFeedback" class="btn-primary" style="width:40%; height: 50px; vertical-align: middle; border-radius: 0.5em; margin-top: 15px;">Write a feedback</button>' + 
+			'</div>';	
+		}	
 	} else {
-		var sReviewHTML = '<div id="idReview1">  <div><div style="width:100%; text-align: center;">' + 
-		'<button id="idWriteFeedback" class="btn-primary" style="width:90%; height: 50px; vertical-align: middle; border-radius: 0.5em; margin-top: 15px;">Write a feedback</button>' +
-		'</div>' + 
-		'<div style="width:100%; text-align: left; padding-left: 24px; padding-top: 30px; border-bottom: 2px solid lightgrey;">' + 
+		var sReviewHTML = '<div id="idReview1">  <div>'; 
+		
+		if (bAllowReview){
+			sReviewHTML = sReviewHTML + '<div style="width:100%; text-align: center;">' + 
+			'<button id="idWriteFeedback" class="btn-primary" style="width:90%; height: 50px; vertical-align: middle; border-radius: 0.5em; margin-top: 15px;">Your turn! Review ' + GetProfileNik() + '</button>' +
+			'</div>';			
+		}	
+		
+		sReviewHTML = sReviewHTML + '<div style="width:100%; text-align: left; padding-left: 24px; padding-top: 30px; border-bottom: 2px solid lightgrey;">' + 
 		'<h5 style="font-weight: bold;">Detailed ratings</h5>' + 
 		'<div id="idRating" style="color: #32CD32;"></div>' +
 		'</div></div>';
@@ -50,37 +88,49 @@ function ReceiveCallback(obj){
 		var TolalRaiting = 0;
 		
 		for (var i = obj.records.length - 1; i >= 0; i--){
-			TolalRaiting = TolalRaiting + obj.records[i].fields.Rating;
+			if (obj.records[i].fields.Rating){			
+				TolalRaiting = TolalRaiting + obj.records[i].fields.Rating;
 			
-			var sGurrRatingImg = '';
-			var sGurrRatingText = '';
+				var sGurrRatingImg = '';
+				var sGurrRatingText = '';
 			
-			switch (obj.records[i].fields.Rating) {
-				case 1:
-					sGurrRatingImg = chrome.extension.getURL('images/cross-mark.png');
-					sGurrRatingText = 'Bad';
-					break;
-				case 2:
-					sGurrRatingImg = chrome.extension.getURL('images/ok-hand.png');
-					sGurrRatingText = 'Okay';
-					break;
-				case 3:
-					sGurrRatingImg = chrome.extension.getURL('images/check-mark-button.png');
-					sGurrRatingText = 'Good';
-					break;
-			}		
+				switch (obj.records[i].fields.Rating) {
+					case 1:
+						sGurrRatingImg = chrome.extension.getURL('images/cross-mark.png');
+						sGurrRatingText = 'Bad';
+						break;
+					case 2:
+						sGurrRatingImg = chrome.extension.getURL('images/ok-hand.png');
+						sGurrRatingText = 'Okay';
+						break;
+					case 3:
+						sGurrRatingImg = chrome.extension.getURL('images/check-mark-button.png');
+						sGurrRatingText = 'Good';
+						break;
+				}	
+
+				var sPublicReview = 'no comment left';
+				if (obj.records[i].fields.PublicReview){
+					sPublicReview = obj.records[i].fields.PublicReview;
+				}
 			
-			var sOneReview = '<div style="width:100%; text-align: left; padding-left: 24px; border-bottom: 1px solid lightgrey;">' +
-			'<div class="fc-default font-weight-bold" style="margin-top: 10px;">' + obj.records[i].fields.Author + '</div>' + 
-			'<div style="vertical-align: middle;"><img src="' + sGurrRatingImg + '" width="14px" height="14px" style="margin-right: 8px;"><div style="display : inline-block">' + sGurrRatingText + '</div></div>' + 
-			'<h5 style="font-weight: bold;">' + obj.records[i].fields.Title + '</h5>' + 	
-			'<div class="roboto-regular mt-1" style="overflow-wrap: anywhere; -ms-word-break: break-all; word-break: break-all; word-break: break-word; outline: none;" tabindex="0">' + obj.records[i].fields.PublicReview + '</div>' +
-			'</div>';			
+				var sReviewTitle = "";
+				if (obj.records[i].fields.Title){
+					sReviewTitle = obj.records[i].fields.Title;
+				}			
 			
-			sReviewHTML = sReviewHTML + sOneReview;	
+				var sOneReview = '<div style="width:100%; text-align: left; padding-left: 24px; border-bottom: 1px solid lightgrey;">' +
+				'<div class="fc-default font-weight-bold" style="margin-top: 10px;">' + obj.records[i].fields.Author + '</div>' + 
+				'<div style="vertical-align: middle;"><img src="' + sGurrRatingImg + '" width="14px" height="14px" style="margin-right: 8px;"><div style="display : inline-block">' + sGurrRatingText + '</div></div>' + 
+				'<h5 style="font-weight: bold;">' + sReviewTitle + '</h5>' + 	
+				'<div class="roboto-regular mt-1" style="overflow-wrap: anywhere; -ms-word-break: break-all; word-break: break-all; word-break: break-word; outline: none;" tabindex="0">' + sPublicReview + '</div>' +
+				'</div>';			
+			
+				sReviewHTML = sReviewHTML + sOneReview;	
+			}
 		}
 		
-		raiting = (((TolalRaiting/obj.records.length)/3)*5).toFixed(1);
+		raiting = (((TolalRaiting/iPublicReviewCount)/3)*5).toFixed(1);
 		
 		console.log('TolalRaiting=' + TolalRaiting);
 		console.log('raiting=' + raiting);
@@ -88,6 +138,7 @@ function ReceiveCallback(obj){
 		sReviewHTML = sReviewHTML + '</div>'; 
 	}
 	
+	if (bAllowReview){
 		var sAddReview1 = '<div id = "idAddReview1" style="display: none; width:95%; border: 2px solid lightgrey; border-radius: 0.8em; text-align: center; height: 230px; padding-top: 50px; margin-left: auto; margin-right: auto; margin-top: 20px;">' + 
 		'<div style="font-size: x-large; font-weight: bold; line-height: normal; margin-bottom: 20px;">How did your interaction<br> with the person go?</div>' + 
 		'<div>' + 
@@ -110,9 +161,11 @@ function ReceiveCallback(obj){
 		
 		'</div>';	
 	
-		$id("idCloutReviews").innerHTML = sReviewHTML + sAddReview1 + sAddReview2;
+		$id("idCloutReviews").innerHTML = sPrivateReviewHTML + sReviewHTML + sAddReview1 + sAddReview2;
 		
-		if (obj.records.length > 0){
+		$id("idPrivateReviewText").value = sPrivateReviewText;
+		
+		if (iPublicReviewCount > 0){
 			starsCount = Math.round(raiting);
 			var ratingText = "";
 			for (var j=1; j <= 5; j++){
@@ -126,9 +179,10 @@ function ReceiveCallback(obj){
 			$id("idRating").textContent = ratingText + " " + raiting.toString();
 		}
 		
-		$id("idWriteFeedback").onclick = function() {
+		$id("idWriteFeedback").onclick = async function() {
 			$id("idReview1").style.display = "none";
 			$id("idAddReview1").style.display = "block";
+			
 		}	
 
 		$id("idButtonGood").onclick = function() {
@@ -155,11 +209,15 @@ function ReceiveCallback(obj){
 			$id("idRatingIcon").src = chrome.extension.getURL('images/cross-mark.png');
 		}
 
-		$id("idPostFeedback").onclick = SaveReview;		
+		$id("idPostFeedback").onclick = SavePublicReview;	
+		
+		$id("idPrivateReviewText").onchange = PrivateReviewChange;	
+		
+	}
 }
 
 
-function AddOurTab(){
+async function AddOurTab(){
 	
 	var tabSelectorEl = document.querySelectorAll('tab-selector')[0];
 	var parentEl = tabSelectorEl.firstChild;
@@ -201,28 +259,70 @@ function AddOurTab(){
 			sCurrUser = elementCurrUser.textContent.trim();
 		}
 		
+	sAuthorPublicKey = await GetLoggedUserPublicKey(GetLoggedUserNik());
+	
+	sPrivateReviewRecordID = "";
+	sPrivateReviewText = "";
+
+	console.log('sAuthorPublicKey =' + sAuthorPublicKey);
+		
 	var msgObj = {};
 	msgObj.msg = 'CLOUTREVIEW_GET_REVIEWS';
 	msgObj.userNik = GetProfileNik();
+	msgObj.userPublicKey = GetProfilePublicKey();
 	
 	chrome.extension.sendMessage(msgObj, ReceiveCallback);				
- 
 }
 
 
-function SaveReview(){
-	//console.log('SaveReview');
+function PrivateReviewChange(){
+	console.log('PrivateReviewChange');
+	if (sPrivateReviewRecordID != ""){
+		UpdatePrivateReview();
+	} else {
+		SavePrivateReview();
+	}	
+}
+
+
+function SavePrivateReview(){
+	var msgObj = {};
+	msgObj.msg = 'CLOUTREVIEW_SAVE_PRIVATEREVIEW';
+	msgObj.saveObj = {};
+	msgObj.saveObj.author = $id("idCurrUser").textContent;
+	msgObj.saveObj.authorPublicKey = sAuthorPublicKey;
+	msgObj.saveObj.reviewedusername = GetProfileNik();	
+	msgObj.saveObj.revieweduserPublicKey = GetProfilePublicKey();	
+	msgObj.saveObj.privateReviewText = $id("idPrivateReviewText").value;	
+	chrome.extension.sendMessage(msgObj);		
+}
+
+
+function UpdatePrivateReview(){
+	var msgObj = {};
+	msgObj.msg = 'CLOUTREVIEW_UPDATE_PRIVATEREVIEW';
+	msgObj.saveObj = {};
+	msgObj.saveObj.recordID = sPrivateReviewRecordID;	
+	msgObj.saveObj.privateReviewText = $id("idPrivateReviewText").value;	
+	chrome.extension.sendMessage(msgObj);		
+}
+
+
+function SavePublicReview(){
+	//console.log('SavePublicReview');
 	
 	var msgObj = {};
 	msgObj.msg = 'CLOUTREVIEW_SAVE_REVIEW';
 	msgObj.saveObj = {};
 	msgObj.saveObj.iRating = iRating;
 	msgObj.saveObj.author = $id("idCurrUser").textContent;
+	//msgObj.saveObj.authorPublicKey = await GetLoggedUserPublicKey(GetLoggedUserNik());
+	msgObj.saveObj.authorPublicKey = sAuthorPublicKey;
 	msgObj.saveObj.reviewedusername = GetProfileNik();
+	msgObj.saveObj.revieweduserPublicKey = GetProfilePublicKey();
 	msgObj.saveObj.title = $id("idReviewTitle").value;
 	msgObj.saveObj.reviewtext = $id("idReviewText").value;
 	
-
 	chrome.extension.sendMessage(msgObj);	
 	
 	$id("idPostFeedback").disabled = true;
@@ -230,11 +330,12 @@ function SaveReview(){
 	
 	$id("idCloutReviews").innerHTML = '';
 	
-	var msgObj = {};
-	msgObj.msg = 'CLOUTREVIEW_GET_REVIEWS';
-	msgObj.userNik = GetProfileNik();
+	var msgObj2 = {};
+	msgObj2.msg = 'CLOUTREVIEW_GET_REVIEWS';
+	msgObj2.userNik = GetProfileNik();
+	msgObj2.userPublicKey = GetProfilePublicKey();	
 	
-	chrome.extension.sendMessage(msgObj, ReceiveCallback);	
+	chrome.extension.sendMessage(msgObj2, ReceiveCallback);	
 }
 
 
@@ -245,6 +346,64 @@ function GetProfileNik(){
 	}
 	return  res;
 }	
+
+
+function GetProfilePublicKey(){
+	var res = "";
+	var elementPublicKey = document.querySelector('i.fa-key');
+	if (elementPublicKey){
+		res = elementPublicKey.parentElement.textContent.trim();
+	}	
+	return  res;
+}
+
+
+function GetLoggedUserNik(){
+	var res = "";	
+	var elementCurrUser = document.querySelector('#changeAccountSelectorRoot');
+		if (elementCurrUser){
+			res = elementCurrUser.textContent.trim();
+		}	
+	return  res;		
+}
+
+
+async function GetLoggedUserPublicKey(sNik){
+    return new Promise(function (resolve, reject) {
+        var requestUrl = "https://api.bitclout.com/get-single-profile";
+			
+		var request = new XMLHttpRequest();		
+		request.open("POST", requestUrl, true);	
+		request.setRequestHeader("Content-type", "application/json");
+		request.withCredentials = true;
+		
+		request.onload = function () {
+			if (request.status = 200) {
+				var tobj = JSON.parse(request.responseText);
+				console.log(tobj);
+				resolve(tobj.Profile.PublicKeyBase58Check);				
+			} else {
+				console.log("Error GetLoggedUserPublicKey" + request.status + " " + request.statusText);
+				reject(false);
+			}		
+		}
+
+		request.onerror = function () {
+			console.log("api.bitclout. Error while GetLoggedUserPublicKey. Request failed");
+			reject(false);
+		};	
+
+			var fields_ = {};
+			fields_["PublicKeyBase58Check"] = "";
+			fields_["Username"] = sNik;
+	
+		var postbody = JSON.stringify(fields_);
+		
+		console.log(postbody);
+  	
+		request.send(postbody);		
+    });	
+}
 
 
 function FirstTab1Click(e){
